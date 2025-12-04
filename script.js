1987669d0cb73a42a6c6eb31d92a7b7987c47958
Khushi5612 <itzkhush2024@gmail.com>
2025-12-04 18:45:55 -0500

    adding the code to give the reference to the form and input

---
+++ script.js
@@ -0,0 +1,7 @@
+const form = document.getElementById('search-form');
+const input = document.getElementById('pokemon-input');
+const statusMessage = document.getElementById('status-message');
+const resultDiv = document.getElementById('result');
+const randomBtn = document.getElementById('random-btn');
+
+const API_BASE_URL = 'https://pokeapi.co/api/v2/pokemon/';
